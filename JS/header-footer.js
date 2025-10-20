41e6af850df6df5f4457ae43f89ab61a09bdffb4
Sara_Meriem_Abdelhafid <sara23.abdelhafid@gmail.com>
2025-10-20 19:40:16 +0100

    new updates10-20-2025

--- JS/header-footer.js
+++ JS/header-footer.js
@@ -1,20 +1,25 @@
 // Header (top menu)
 document.getElementById("header").innerHTML = `
   <header class="top-bar">
-    <img src="src/Test_Logo.png" alt="Logo" class="logo">
+    <!--<img src="src/Test_Logo.png" alt="Logo" class="logo">-->
+    <!--<div class="logo">Wassim El Madina</div>-->
+    <div class="logo">
+      <div class="line1">Wassim</div>
+      <div class="line2">El Madina</div>
+    </div>
     <nav class="menu">
-      <a href="#" onclick="loadPage('home')">Home</a>
-      <a href="#" onclick="loadPage('projects')">Projects</a>
+      <a href="javascript:void(0)" onclick="loadPage('home')">Home</a>
+      <a href="javascript:void(0)" onclick="loadPage('projects')">Projects</a>
       <a href="#" onclick="loadPage('about')">About</a>
       <a href="#" onclick="loadPage('contact')">Contact</a>
     </nav>
   </header>
 `;
 
-
+//"#" && "javascript:void(0)" means “stay on the same page”
 // Footer (bottom contact info)
-document.getElementById("footer").innerHTML = `
-  <footer class="bottom-bar">
-    <p>© 2025 Wassim El Madina Bridge | Contact: info@madinabridge.com</p>
+document.getElementById('footer').innerHTML = `
+  <footer class="site-footer">
+    <p>&copy; 2025 Wassim El Madina Construction Co. | All Rights Reserved</p>
   </footer>
 `;
