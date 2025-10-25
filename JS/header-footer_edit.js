c350ef2d6c98ade674fe16a8678394d5df7769dd
Sara_Meriem_Abdelhafid <sara23.abdelhafid@gmail.com>
2025-10-25 21:05:27 +0100

    update

---
+++ JS/header-footer_edit.js
@@ -0,0 +1,25 @@
+// Header (top menu)
+document.getElementById("header").innerHTML = `
+  <header class="top-bar">
+    <!--<img src="src/Test_Logo.png" alt="Logo" class="logo">-->
+    <!--<div class="logo">Wassim El Madina</div>-->
+    <div class="logo">
+      <div class="line1">Wassim</div>
+      <div class="line2">El Madina</div>
+    </div>
+    <nav class="menu">
+      <a href="javascript:void(0)" onclick="loadPage('home_edit')">Home</a>
+      <a href="javascript:void(0)" onclick="loadPage('projects_edit')">Projects</a>
+      <a href="#" onclick="loadPage('about_edit')">About</a>
+      <a href="#" onclick="loadPage('contact_edit')">Contact</a>
+    </nav>
+  </header>
+`;
+
+//"#" && "javascript:void(0)" means “stay on the same page”
+// Footer (bottom contact info)
+document.getElementById('footer').innerHTML = `
+  <footer class="site-footer">
+    <p>&copy; 2025 Wassim El Madina Construction Co. | All Rights Reserved</p>
+  </footer>
+`;
