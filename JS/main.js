7f318f0f05c85a8802a32146b5acd910dfbb787f
Sara_Meriem_Abdelhafid <sara23.abdelhafid@gmail.com>
2026-02-09 04:42:04 +0100

    delzte repitation

--- JS/main.js
+++
@@ -1,7 +0,0 @@
-function loadPage(page) {
-  document.getElementById('contentFrame').src = `HTML/${page}.html`;
-}
-// Toggle menu visibility on small screens
-document.getElementById("menuToggle").addEventListener("click", () => {
-  document.getElementById("menu").classList.toggle("show");
-});
